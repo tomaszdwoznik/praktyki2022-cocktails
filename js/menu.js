d4c2c6e57c95e12874564d84759fb2f1a601a135
unknown <tomek.dwoznik@icloud.com>
2022-02-08 18:01:54 +0100

    #2 - praca nad RWD topu

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     $(".top__burger").on("click", function(){
-
+        
         $(".container--list").toggleClass("open");
         $(".top__menu ul").toggleClass("open");
     });
