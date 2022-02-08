a8544b3a06616556fd51d9e590359f6d61d8bd9c
unknown <tomek.dwoznik@icloud.com>
2022-02-08 11:01:14 +0100

    #2 - dzialajace burger-menu

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+    $(".top__burger").on("click", function(){
+
+        $(".container--list").toggleClass("open");
+        $(".top__menu ul").toggleClass("open");
+    });
+
+});
