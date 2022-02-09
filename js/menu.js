692c906e799d4089004323ad104b756df68c1710
unknown <tomek.dwoznik@icloud.com>
2022-02-09 12:30:42 +0100

    #2 - dzialajacy top

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,27 @@
 function hamburgerMenu(){
-    var width = document.getElementById("top__burger--height")
-    if(width.className == "container--list"){
-        width.className = "container--list.open";
+    var container = document.getElementsByClassName("container--list")[0];
+    if(container.style.height == "0px"){
+        container.style.height = "200px";
     }
     else{
-        width.className = "container--list"
+        container.style.height = "0px";
+    }
+}
+
+function search(){
+    var list = document.getElementsByClassName("top__burger")[0];
+    var text = document.getElementsByClassName("top__burger__input")[0];
+    if(list.style.height == "0px"){
+        list.style.height = "40px";
+    }
+    else{
+        list.style.height = "0px";
+    }
+
+    if(text.style.height == "0px"){
+        text.style.height = "25px";
+    }
+    else{
+        text.style.height = "0px";
     }
 }
